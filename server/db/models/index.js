8954054b00017a0d3d7fe05cbd87f361914b64e0
Lucas Curti <lucas.curti@gmail.com>
2018-03-06 15:08:38 -0300

    Update index.js

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,14 +2,14 @@ const User = require('./users');
 const Review = require('./reviews');
 const Product = require('./products');
 const Order = require('./orders');
-const Categorie = require('./categories');
+const Category = require('./category');
 
 User.hasMany(Review, { as: 'review' });
 User.hasMany(Order, { as: 'orders' });
 Review.belongsTo(Product, { as: 'product' });
 Review.belongsTo(User, { as: 'user' });
 Product.belongsToMany(Order, { as: 'orders', through: 'order_product' });
-Product.belongsTo(Categorie, { as: 'categorie' });
+Product.belongsTo(Category);
 Product.hasMany(Review, { as: 'reviews' });
 Order.belongsToMany(Product, { as: 'product', through: 'order_product' });
 Order.belongsTo(User, { as: 'user' });
@@ -19,5 +19,5 @@ module.export = {
   Review,
   Product,
   Order,
-  Categorie,
+  Category,
 };
