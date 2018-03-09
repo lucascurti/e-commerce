c72c447f5873073447b0a32e9a7614bcde4a24e6
MeliSayago <melisayago@gmail.com>
2018-03-09 17:29:49 -0300

    merge

--- client/react/reducers/products-reducer.js
+++ client/react/reducers/products-reducer.js
@@ -1,10 +1,14 @@
-import { GET_PRODUCTS } from '../constants';
+import { GET_PRODUCTS, GET_PRODUCTS_CATEGORY } from '../constants';
 
 const initialState = [];
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case GET_PRODUCTS:
+      return action.products;
+  }
+  switch (action.type) {
+    case GET_PRODUCTS_CATEGORY:
       return action.products;
     default:
       return state;
